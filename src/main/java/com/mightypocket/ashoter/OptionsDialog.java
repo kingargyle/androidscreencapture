e72c22371d7d56f3ed9f0bd8b0a647cfb54c625b
etf <etf@b3acf445-3cf9-441e-a8c5-06f47e220dd9>
2010-09-29 10:09:11 +0000

    Option dialog update
    git-svn-id: http://svn.code.sf.net/p/ashot/code/ashoter/trunk@5 b3acf445-3cf9-441e-a8c5-06f47e220dd9

--- src/main/java/com/mightypocket/ashoter/OptionsDialog.java
+++ src/main/java/com/mightypocket/ashoter/OptionsDialog.java
@@ -6,7 +6,9 @@ package com.mightypocket.ashoter;
 
 import java.awt.BorderLayout;
 import java.util.prefs.Preferences;
+import javax.swing.GroupLayout;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -19,10 +21,12 @@ import org.jdesktop.application.ResourceMap;
  *
  * @author Illya Yalovyy
  */
-public final class OptionsDialog extends JDialog {
+public final class OptionsDialog extends JDialog implements PreferencesNames {
     private final Mediator mediator;
     private final Preferences p = Preferences.userNodeForPackage(AShoter.class);
     private boolean ok;
+
+    private JCheckBox showLabelsInToolbarCheckBox;
 
     public OptionsDialog(Mediator mediator) {
         super(mediator.getApplication().getMainFrame(), true);
@@ -33,10 +37,12 @@ public final class OptionsDialog extends JDialog {
         resourceMap.injectComponents(this);
 
         loadPreferences();
+        this.pack();
     }
 
     private void initComponents() {
         ApplicationActionMap actionMap = mediator.getApplication().getContext().getActionMap(this);
+        ResourceMap resourceMap = mediator.getApplication().getContext().getResourceMap(OptionsDialog.class);
         getRootPane().setLayout(new BorderLayout());
 
         JPanel buttonsPanel = new JPanel();
@@ -44,10 +50,42 @@ public final class OptionsDialog extends JDialog {
         buttonsPanel.add(new JButton(actionMap.get(ACTION_CANCEL)));
 
         JPanel generalPanel = new JPanel();
-        generalPanel.add(new JLabel("General"));
+        GroupLayout gl1 = new GroupLayout(generalPanel);
+        generalPanel.setLayout(gl1);
+        gl1.setAutoCreateGaps(true);
+        gl1.setAutoCreateContainerGaps(true);
+
+        JLabel sdkPathLabel = new JLabel();
+        sdkPathLabel.setName("sdkPathLabel");
+        JButton setSdkPathButton = new JButton(actionMap.get(ACTION_SET_SDK_PATH));
+        showLabelsInToolbarCheckBox = new JCheckBox();
+        showLabelsInToolbarCheckBox.setName("showLabelsInToolbarCheckBox");
+
+        gl1.setHorizontalGroup(gl1.createParallelGroup()
+            .addGroup(gl1.createSequentialGroup()
+                .addComponent(sdkPathLabel)
+                .addComponent(setSdkPathButton)
+                )
+            .addComponent(showLabelsInToolbarCheckBox)
+            );
+        gl1.setVerticalGroup(gl1.createSequentialGroup()
+            .addGroup(gl1.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                .addComponent(sdkPathLabel)
+                .addComponent(setSdkPathButton)
+                )
+            .addComponent(showLabelsInToolbarCheckBox)
+            );
+        JPanel fullScreenPanel = new JPanel();
+
+        JPanel savePanel = new JPanel();
+
+        JPanel updatePanel = new JPanel();
 
         JTabbedPane pane = new JTabbedPane();
-        pane.addTab("General", generalPanel);
+        pane.addTab(resourceMap.getString("tab.general"), generalPanel);
+        pane.addTab(resourceMap.getString("tab.fullScreen"), fullScreenPanel);
+        pane.addTab(resourceMap.getString("tab.saving"), savePanel);
+        pane.addTab(resourceMap.getString("tab.updating"), updatePanel);
 
 
         getRootPane().add(pane, BorderLayout.CENTER);
@@ -68,6 +106,12 @@ public final class OptionsDialog extends JDialog {
     public void actionCancel() {
         ok = false;
         setVisible(false);
+    }
+
+    public static final String ACTION_SET_SDK_PATH = "setSdkPath";
+    @Action(name=ACTION_SET_SDK_PATH)
+    public void setSdkPath() {
+
     }
 
     private void loadPreferences() {
